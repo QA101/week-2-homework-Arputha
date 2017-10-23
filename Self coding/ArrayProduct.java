afe77b5075db59b900c0903b538ce90632165d8e
Arputha <32491918+Arputha@users.noreply.github.com>
2017-10-22 20:56:01 -0700

    Create ArrayProduct.java

---
+++ Self coding/ArrayProduct.java
@@ -0,0 +1,17 @@
+
+public class ArrayProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a = {1,2,3,4};
+		int product = 1;
+		for(int elements: a) {
+			product *= elements;
+		}
+			System.out.println( a[0]+"*"+a[1]+"*"+a[2]+"*"+a[3]+" = "+ product );
+			
+		
+
+	}
+
+}
