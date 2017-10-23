f07b1467d98cc4fb182ad9144c0ffa08d25b9ba6
Arputha <32491918+Arputha@users.noreply.github.com>
2017-10-22 20:47:53 -0700

    Create Forloops.java

---
+++ Self coding/Forloops.java
@@ -0,0 +1,52 @@
+
+public class Forloops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<4;i++) {
+			
+				System.out.println("****** " );
+			}
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<=i;j++) {
+			System.out.print(" 2");
+			}
+			System.out.println();
+		}
+		for(int i=0;i<8;i+=2) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(" $");
+				
+			}
+			System.out.println( "");
+		}
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<=i;j++) {
+			System.out.print("**");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<5;i++) {
+			for(int j=1;j<=i;j++) {
+			System.out.print(" "+j);
+			}
+			System.out.println();
+		}
+		
+		
+		for(int i=3;i<=7;i+=2) {
+			for(int j=1;j<=i;j++) {
+			System.out.print(" "+j);
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+			System.out.print(" "+i);
+			}
+			System.out.println(" "+i);
+		}
+		}
+	
+
+	}
