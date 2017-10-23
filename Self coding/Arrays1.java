da1f192f75803faeee92769eae025a6d23b66867
Arputha <32491918+Arputha@users.noreply.github.com>
2017-10-22 20:56:45 -0700

    Create Arrays1.java

---
+++ Self coding/Arrays1.java
@@ -0,0 +1,18 @@
+
+public class Arrays1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a ={1,2,3,4};
+		
+		for(int i=0;i< 4; i++) {
+			System.out.println("a["+i+"] = "+a[i]);
+			
+		}
+		
+		
+		
+
+	}
+
+}
