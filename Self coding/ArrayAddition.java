2b48723fc27673149308501ba5a6cc02f3783713
Arputha <32491918+Arputha@users.noreply.github.com>
2017-10-22 22:17:33 -0700

    Create ArrayAddition.java

---
+++ Self coding/ArrayAddition.java
@@ -0,0 +1,23 @@
+
+public class ArrayAddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a ={1,2,3,4};
+		int [] b = {10,20,30,40};
+		int [] c=new int[4];
+		for (int i=0;i<a.length;i++) {
+			c[i]=a[i]+b[i];
+		}
+		printArray(a);
+		printArray(b);
+		printArray(c);
+	}
+	private static void printArray(int[] a) {
+		for (int i=0;i<a.length;i++) {
+			System.out.print(a[i]+" ");
+		}
+		System.out.println("\n");
+	}
+
+}
