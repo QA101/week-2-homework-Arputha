425326af5275f74acd100fdc3f5bc7458cb43079
Arputha <32491918+Arputha@users.noreply.github.com>
2017-10-22 20:52:46 -0700

    Create ArrayDouble1.java

---
+++ Self coding/ArrayDouble1.java
@@ -0,0 +1,13 @@
+
+public class ArrayDouble1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 double[] a ={1.2, 2.5, 3.8, -12};
+		 for(int i=0;i<4;i++) {
+			 System.out.println("a["+i+"] = "+a[i]);
+		 }
+
+	}
+
+}
